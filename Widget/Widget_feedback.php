cdff848cd9eaa2314876f4b494af6ec844c69602
Clément <108410281+cbaraise@users.noreply.github.com>
2023-03-14 11:28:03 +0100

    changelent de palce du css du widget et réorganisation

--- Widget/Widget_feedback.php
+++ Widget/Widget_feedback.php
@@ -13,16 +13,27 @@ function __construct() {
 // Fonction pour afficher le contenu du widget
 public function widget( $args, $instance ) {
     echo $args['before_widget'];
-    echo $args['before_title'] . 'Avis client ' . $args['after_title'];
+    echo $args['before_title'] .'<h1> Avis client </h1>' . $args['after_title'];
 
     // Récupérer les éléments de la table en base de données
     global $wpdb; // Utiliser la global $wpdb pour exécuter des requêtes SQL
     $results = $wpdb->get_results( "SELECT * FROM wp_notifandfeedback ORDER BY wp_notifandfeedback .date_avis DESC;");
     // Afficher les éléments récupérés
     if ( $results ) {
-        foreach ( $results as $result ) {
-            echo '<p>' . $result->titre . '</p>';
-        }
+		echo '<div class="avis-clients-container">';
+		echo '<div class="avis-clients">';
+		foreach ( $results as $result ) {
+			$note = intval( $result->note );
+			$etoiles = str_repeat( '&#9733;', $note ) . str_repeat( '&#9734;', 5 - $note );
+	
+			// Affichage du commentaire avec la note en étoiles
+			echo '<div class="avis-client">';
+			echo '<p class="nom-utilisateur">' . $result->nom_utilisateur. ' - ' . $result->date_avis  . '</p>';
+			echo '<p class="note">' . $etoiles . '</p>';
+			echo '<p class="commentaire">' . $result->commentaire . '</p>';
+			echo '</div>';
+			
+		}
     } else {
         echo '<p>Aucun élément à afficher</p>';
     }
@@ -37,10 +48,9 @@ public function form( $instance ) {
 
 }
 
-// Enregistrement du widget
-function register_mon_widget() {
-register_widget( 'Mon_Widget' );
+// Enregistrer un style personnalisé pour le widget
+function enregistrer_style_widget_custom() {
+    wp_enqueue_style( 'widget-style', plugins_url( 'NotifBooking\src\css\widget-style.css') );
 }
-add_action( 'widgets_init', 'register_mon_widget' );
-
+add_action( 'wp_enqueue_scripts', 'enregistrer_style_widget_custom' );
 ?>
